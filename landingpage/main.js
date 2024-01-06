9cb65e98e2c34f3d6f40781a83e76416a81c4d29
simbados <22945705+simbados@users.noreply.github.com>
2024-01-06 13:54:36 +0100

    Add project section and change styling

--- landingpage/main.js
+++ landingpage/main.js
@@ -51,60 +51,116 @@ document.addEventListener('DOMContentLoaded', (event) => {
     function addObserver(el, options) {
         const observer = new IntersectionObserver(function (entries, observer) {
             entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    if(options.cb) {
-                        options.cb(el);
-                    }
+                if (entry.isIntersecting && options.cb) {
+                    options.cb(el);
                 }
             });
         }, options);
         observer.observe(el);
     }
+
+    const welcomeSection = document.querySelector('#welcome-id');
+
+    let runWelcomeSection = true;
+    addObserver(welcomeSection, {
+        threshold: 0.3,
+        cb: function(el){
+            if (runWelcomeSection) {
+                const welcome = el.querySelector('#welcome-message-id');
+                const welcomeText = "Hello and welcome stranger.\nNice to have you.\nPlease scroll on :)";
+                typeText(welcome, welcomeText);
+                runWelcomeSection = false;
+            }
+        },
+    })
+
     const skillSection = document.querySelector('#skill-section');
 
+    let runSkillSection = true;
     addObserver(skillSection, {
-        threshold: 0.05,
+        threshold: 0.3,
         cb: function(el){
-            for (const [index, child] of Object.entries(el.children)) {
-                child.style.animation = 'none';
+            if (runSkillSection) {
+                runSkillSection = false;
                 setTimeout(() => {
-                    if (child.classList.contains('skill-low')) {
-                        child.style.animation = `fade-in-4 ${index/2}s`;
-                    } else {
-                        child.style.animation = `fade-in-2 ${index/2}s`;
-                    }
-                }, 20)
+                    el.children[0].style.display = 'flex';
+                }, 200);
             }
-        }
+        },
     })
 
     const contactSection = document.querySelector('#contact');
+    let runContactSection = true;
     addObserver(contactSection, {
         threshold: 0.3,
         cb: function(el) {
-            setTimeout(() => {
-                typeText(el.querySelector('#contact-message-id'), 'Contact me at:\ninfo(at)simbados.com')
-            }, 100);
+            if (runContactSection) {
+                runContactSection = false;
+                setTimeout(async () => {
+                    await typeText(el.querySelector('#contact-message-id'), 'Contact me at:\ninfo(at)simbados.com')
+                    await wait(500);
+                    el.querySelector('#contact-message-id').classList.remove('contact-message-active');
+                }, 300);
+            }
         }
     });
 
+    const projectSection = document.querySelector('#projects');
+    let runProjectSection = true;
+    addObserver(projectSection, {
+        threshold: 0.3,
+        cb: function(el) {
+            if (runProjectSection) {
+                runProjectSection = false;
+                setTimeout(async () => {
+                    el.querySelector('#project-message-intro').classList.add('project-message-intro-active');
+                    await typeText(el.querySelector('#project-message-intro'), 'Current projects:\n\nGolang:')
+                    el.querySelector('#project-message-intro').classList.remove('project-message-intro-active')
+                    el.querySelector('#project-agey').style.display = 'block';
+                    await wait(500);
+                    el.querySelector('#project-sb').style.display = 'block';
+                    await wait(500);
+                    el.querySelector('#project-message-js').classList.add('project-message-js-active');
+                    await typeText(el.querySelector('#project-message-js'), 'Javascript:')
+                    el.querySelector('#project-message-js').classList.remove('project-message-js-active');
+                    el.querySelector('#project-website').style.display = 'block';
+                    await wait(500);
+                    el.querySelector('#project-message-others').classList.add('project-message-others-active');
+                    await typeText(el.querySelector('#project-message-others'), 'Others:')
+                    el.querySelector('#project-message-others').classList.remove('project-message-others-active');
+                    el.querySelector('#project-katas').style.display = 'block';
+                }, 50);
+            }
+        },
+    });
 
-    function typeText(el, text) {
+
+    async function typeText(el, text, newText = true) {
         let index = 0;
 
-        el.textContent = '';
-        function type() {
-            el.textContent += text[index];
-            index++;
-
-            if (index < text.length) {
-                setTimeout(type, 50);
-            }
+        if (newText) {
+            el.textContent = '';
         }
+        return new Promise((resolve) => {
+            function type() {
+                el.textContent += text[index];
+                index++;
 
-        type();
+                if (index < text.length) {
+                    setTimeout(type, 50);
+                } else if (index === text.length) {
+                    resolve();
+                }
+            }
+            type();
+        })
     }
-    const welcome = document.getElementById('welcome-message-id');
-    const welcomeText = "Hello and welcome stranger.\nNice to have you.\nPlease scroll on :)";
-    typeText(welcome, welcomeText);
+
 })
+async function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+}
